43e03d36ee5187e3e0d93e9fdb87ccfa867a26d3
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-11-06 19:52:56 +0530

    download stopwords

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -3,10 +3,12 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
-# nltk.download('stopwords')
+nltk.download('stopwords')
+
 
 def preprocess_text(text):
     lemma = WordNetLemmatizer()
     text = re.sub('[^a-zA-Z]', ' ', text).lower()
-    text = ' '.join([lemma.lemmatize(word) for word in text.split() if word not in stopwords.words('english')])
+    text = ' '.join([lemma.lemmatize(word) for word in text.split()
+                    if word not in stopwords.words('english')])
     return text
