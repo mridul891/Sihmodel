706c8122c24f3c32dd879948372b1ebf01ac1d75
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-11-07 07:45:20 +0530

    Change in requirements

--- model.py
+++ model.py
@@ -8,13 +8,13 @@ from xgboost import XGBClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import VotingClassifier
 from sklearn.metrics import accuracy_score
-
+import pickle
 # Load and preprocess data
 data = pd.read_csv(r'./trainDisaster.csv')
 data['text'] = data['text'].apply(preprocess_text)
 
 # Vectorize the text
-vectorizer = CountVectorizer(max_features=3000,ngram_range=(1,3))
+vectorizer = CountVectorizer(max_features=3000, ngram_range=(1, 3))
 X = vectorizer.fit_transform(data['text']).toarray()
 
 # Apply PCA
@@ -22,25 +22,31 @@ pca = PCA(n_components=15)
 X_pca = pca.fit_transform(X)
 
 # Train-test split
-X_train, X_test, y_train, y_test = train_test_split(X_pca, data['target'], test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X_pca, data['target'], test_size=0.2, random_state=42)
 
 # Train the model
+
 # model = RandomForestClassifier()
 # model.fit(X_train, y_train)
 
-from sklearn.ensemble import VotingClassifier
 
 ensemble_model = VotingClassifier(estimators=[
-    ('xgb', XGBClassifier(subsample= 0.9, n_estimators= 400, min_child_weight= 3 , max_depth= 9, learning_rate= 0.01, gamma=0, colsample_bytree= 1.0)),
+    ('xgb', XGBClassifier(subsample=0.9, n_estimators=400, min_child_weight=3,
+     max_depth=9, learning_rate=0.01, gamma=0, colsample_bytree=1.0)),
     ('rf', RandomForestClassifier()),
     ('ada', AdaBoostClassifier())
 ], voting='hard')
-ensemble_model.fit(X_train,y_train)
+ensemble_model.fit(X_train, y_train)
 
 y_pred = ensemble_model.predict(X_test)
-acc = accuracy_score(y_test,y_pred)
+acc = accuracy_score(y_test, y_pred)
 
+with open('model.pkl', 'wb') as file:
+    pickle.dump(ensemble_model, file)
 
 # Save the vectorizer, PCA, and model as variables
+
+
 def get_models():
-    return ensemble_model , vectorizer, pca , acc
+    return ensemble_model, vectorizer, pca, acc
